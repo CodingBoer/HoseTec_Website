0640b2ec69643f3074a737db89545e0da6ae4b46
CodingBoer <alainrobinson@MacBookAir.home>
2024-11-20 14:47:39 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// JavaScript for form submission (example)
+document.getElementById('contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Thank you for your message! We will get back to you soon.');
+});
